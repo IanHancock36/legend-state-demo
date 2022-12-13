767af17b682460d50f32212e5f30f25bde1dee0a
Ian Hancock <northroadian@gmail.com>
2022-12-13 06:30:18 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { observable, observe } from "@legendapp/state";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div>BACKGROUND</div>;
-}
+const themeState = observable({ settings: { theme: "dark" } });
+themeState.settings.theme === "dark";
+
+observe(() => {
+  console.log(themeState.settings.theme.get());
+});
+
+const App = observer(function App() {
+  const theme = state.settings.theme.get();
+  const toggleTheme = () => {
+    state.settings.theme.set((theme) => {
+      theme === "dark" ? "light" : "dark";
+    });
+  };
+  return (
+    <div>
+      <div>THeme :{theme}</div>
+      <button onClick={toggleTheme}>TOGGLE THEME </button>
+    </div>
+  );
+});
 
 export default App;
